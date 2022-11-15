890e99bb5a8558e3cdae9b545af0a616dc4be790
Camiel <camiel.eggermont@keystone-wx.com>
2022-11-15 12:14:16 +0100

    Added trapezoid in the commands list

--- Refactoring/CommandParser.cs
+++ Refactoring/CommandParser.cs
@@ -30,6 +30,7 @@ namespace Refactoring
             sb.AppendLine("- create circle {double} (create a new circle)");
             sb.AppendLine("- create rectangle {height} {width} (create a new rectangle)");
             sb.AppendLine("- create triangle {height} {width} (create a new triangle)");
+            sb.AppendLine("- create trapezoid {base1} {base2} {height} (create a new trapezoid)");
             sb.AppendLine("- print (print the calculated surface areas)");
             //sb.AppendLine("- calculate (calulate the surface areas of the created shapes)");
             sb.AppendLine("- reset (reset)");
